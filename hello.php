fd30bf81c0c869606fc1bd5c6d416d270d5c0e6f
RizalFIrdaus <rizal.firdaus117@gmail.com>
2023-01-26 17:15:41 +0700

    v.1.1.0

--- hello.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . "/vendor/autoload.php";
-
-use rizalfirdaus\Belajar\sayHello;
-
-$say = new sayHello("Rizal", 22);
-$say->sayHello();
--- src/Belajar/sayHello.php
+++ src/Belajar/sayHello.php
@@ -2,7 +2,18 @@
 
 namespace rizalfirdaus\Belajar;
 
-class sayHello
+interface Setter
+{
+    public function setName(string $name): void;
+    public function setAge(int $age): void;
+}
+interface Getter
+{
+    public function getName(): string;
+    public function getAge(): int;
+}
+
+class sayHello implements Setter, Getter
 {
     public function __construct(private string $name, private int $age)
     {
@@ -11,5 +22,21 @@ class sayHello
     public function sayHello(): void
     {
         echo "Hello $this->name !, i'am $this->age years old" . PHP_EOL;
+    }
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+    public function setAge(int $age): void
+    {
+        $this->age = $age;
+    }
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    public function getAge(): int
+    {
+        return $this->age;
     }
 }
